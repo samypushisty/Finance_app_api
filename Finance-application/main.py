825666df5e3f97197964362887cad8a1b6575eaf
samypushisty <pomidorik228@gmail.com>
2025-02-24 14:07:49 +0300

    update swagger url

--- Finance-application/main.py
+++ Finance-application/main.py
@@ -9,6 +9,10 @@ from api.api_v1.services.base_schemas.schemas import StandartException
 from core.config import settings
 from core.models import db_helper
 
+from fastapi.openapi.docs import (
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -19,9 +23,24 @@ async def lifespan(app: FastAPI):
 
 main_app = FastAPI(
     lifespan=lifespan,
-    title="Trading App"
+    title="Trading App",
+    docs_url=None, redoc_url=None
 )
 
+@main_app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=main_app.openapi_url,
+        title=main_app.title + " - Swagger UI",
+        oauth2_redirect_url=main_app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
+
+
+@main_app.get(main_app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
 main_app.include_router(
     api_router
 )
