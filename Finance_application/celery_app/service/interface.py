0dd32483c232024a30bc8e8bd22398912f9e6e4d
samypushisty <pomidorik228@gmail.com>
2025-06-20 22:24:58 +0300

    cleaning up unnecessary functionality

--- Finance_application/celery_app/service/interface.py
+++
@@ -1,8 +0,0 @@
-from abc import abstractmethod
-from typing import Protocol, List
-
-
-class TasksI(Protocol):
-    @abstractmethod
-    def get_ids_active_users(self) -> List[int]:
-        ...
--- Finance_application/celery_app/tasks.py
+++ Finance_application/celery_app/tasks.py
@@ -1,12 +1,8 @@
 import requests
 from celery_app.app import celery_app
 from celery_app.config import  settings
-from celery_app.container import container
-from celery_app.service.interface import TasksI
 from core.redis_db.redis_helper import  redis_local_client
 
-def get_tasks_service() -> TasksI:
-    return container.tasks_service()
 
 @celery_app.task(
     name='update_prices',
@@ -20,11 +16,3 @@ def update_prices():
         return "Success"
     except Exception as e:
         return e
-
-# @celery_app.task(
-#     name='update_balances',
-# )
-# def update_balances():
-#     tasks_service = get_tasks_service()
-#     ids = tasks_service.get_ids_active_users()
-#     return ids
